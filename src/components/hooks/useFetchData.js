82d44c4a207150390652ce8da6006547763407fa
Carlos Arteaga <cartegachamorro@gmial.com>
2026-09-23 01:01:12 -0500

    learning customHooks

---
+++ src/components/hooks/useFetchData.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { fetchData } from "../../helpers/fetchData";
+
+export const useFetchData = ({ endPoint }) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData(endPoint).then((res) => {
+      setData(res.data);
+      setIsLoading(res.isLoading);
+    });
+  }, [endPoint]);
+  return { data, isLoading };
+};
